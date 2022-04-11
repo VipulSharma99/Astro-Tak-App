0c37744ca15eb1380e6f59f7066b5bd986677aa7
Vipul <vipulsharma10121999@gmail.com>
2022-04-11 13:05:20 +0530

    adding lazy loading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import { Routes, Route, Link } from "react-router-dom";
-import Questions from './Screens/Questions';
-import Family from './Screens/family';
-import FamilyList from './FamilyComponent/familyList/familyList';
+// import Questions from './Screens/Questions';
+// import Family from './Screens/family';
+// import FamilyList from './FamilyComponent/familyList/familyList';
 import classes from './App.module.css';
 
+const Questions = React.lazy(()=>import('./Screens/Questions'));
+const Family = React.lazy(()=>import('./Screens/family'));
+const FamilyList = React.lazy(()=>import('./FamilyComponent/familyList/familyList'));
+
 function App() {
+
   return (
     <div className={classes.body}>
-      <Routes>
-        <Route path='/' element={<Questions />} />
-        <Route path='/family' element={<Family />}>
-          <Route index element={<FamilyList />} />
-        </Route>
-      </Routes>
+      <Suspense fallback="Loading..">
+        <Routes>
+          <Route path='/' element={<Questions />} />
+          <Route path='/family' element={<Family />}>
+            <Route index element={<FamilyList />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/Screens/family.js
+++ src/Screens/family.js
@@ -1,10 +1,14 @@
-import React,{useState} from 'react';
+import React,{useState, Suspense} from 'react';
 import Header from '../FamilyComponent/familyheader/familyHeader';
 import Navigation from '../FamilyComponent/navigation/Navigation';
-import FamilyList from '../FamilyComponent/familyList/familyList';
+// import FamilyList from '../FamilyComponent/familyList/familyList';
 import {useSelector} from 'react-redux';
-import NewProfile from '../FamilyComponent/newProfile/newProfile';
-import EditProfile from '../FamilyComponent/newProfile/editProfile';
+// import NewProfile from '../FamilyComponent/newProfile/newProfile';
+// import EditProfile from '../FamilyComponent/newProfile/editProfile';
+
+const NewProfile = React.lazy(()=>import('../FamilyComponent/newProfile/newProfile'));
+const EditProfile = React.lazy(()=>import('../FamilyComponent/newProfile/editProfile'));
+const FamilyList = React.lazy(()=>import('../FamilyComponent/familyList/familyList'));
 
 const Family = ()=>{
   const view =useSelector((state) => state.view);
@@ -12,10 +16,12 @@ const Family = ()=>{
     <div style={{itemAlign:'centre',maxWidth:'inherit'}}>
         <Header />
         <Navigation />
-        {view=='profile'?
-          <NewProfile />
-          :(view=='edit'?<EditProfile/>:<FamilyList/>)
-        }
+        <Suspense fallback='Loading...'>
+          {view=='profile'?
+            <NewProfile />
+            :(view=='edit'?<EditProfile/>:<FamilyList/>)
+          }
+        </Suspense>
     </div>
   );
 }
